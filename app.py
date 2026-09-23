5f4e1ec15f9bf174966e12c8719fd03e70c80ea4
playalan99-art <playalan99@gmail.com>
2026-09-23 15:36:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,20 +34,7 @@ PRODUCTS = [
             {"name": "Grande", "price": 6.00},
         ],
     },
-],
-},
-    {
-        "id": "lomo-apanado",
-        "name": "Lomo Apanado ",
-        "description": "Una combinación especial preparada al momento para disfrutar en familia.",
-        "image": "🥢",
-        "variants": [
-            {"name": "Completo", "price": 7.00},
-        ],
-    },
-],
-
-
+]
 @app.route("/")
 def index():
     return render_template("index.html", products=PRODUCTS)
